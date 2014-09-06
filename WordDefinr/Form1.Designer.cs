94aefbf19bfe77de37c868934c8a0611743f309f
rakeshdas1 <rakeshdaskowshik@gmail.com>
2014-09-06 01:13:04 -0500

    Done

--- WordDefinr/Form1.Designer.cs
+++ WordDefinr/Form1.Designer.cs
@@ -28,9 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WordDefinr));
             this.txtWords = new System.Windows.Forms.RichTextBox();
             this.btnDefine = new System.Windows.Forms.Button();
             this.lblCopied = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClr = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtWords
@@ -43,7 +48,7 @@
             // 
             // btnDefine
             // 
-            this.btnDefine.Location = new System.Drawing.Point(105, 179);
+            this.btnDefine.Location = new System.Drawing.Point(19, 180);
             this.btnDefine.Name = "btnDefine";
             this.btnDefine.Size = new System.Drawing.Size(75, 23);
             this.btnDefine.TabIndex = 1;
@@ -59,14 +64,57 @@
             this.lblCopied.Size = new System.Drawing.Size(0, 13);
             this.lblCopied.TabIndex = 2;
             // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(100, 180);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClr
+            // 
+            this.btnClr.Location = new System.Drawing.Point(181, 180);
+            this.btnClr.Name = "btnClr";
+            this.btnClr.Size = new System.Drawing.Size(75, 23);
+            this.btnClr.TabIndex = 4;
+            this.btnClr.Text = "Clear";
+            this.btnClr.UseVisualStyleBackColor = true;
+            this.btnClr.Click += new System.EventHandler(this.btnClr_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter all of your words in the textbox below in separate lines";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Written by Rakesh Das V1.0";
+            // 
             // WordDefinr
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClr);
+            this.Controls.Add(this.btnExit);
             this.Controls.Add(this.lblCopied);
             this.Controls.Add(this.btnDefine);
             this.Controls.Add(this.txtWords);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "WordDefinr";
             this.Text = "Word Definr";
             this.ResumeLayout(false);
@@ -79,6 +127,10 @@
         private System.Windows.Forms.RichTextBox txtWords;
         private System.Windows.Forms.Button btnDefine;
         private System.Windows.Forms.Label lblCopied;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WordDefinr/Form1.cs
+++ WordDefinr/Form1.cs
@@ -23,20 +23,33 @@ namespace WordDefinr
             XmlDocument xDoc = new XmlDocument();
             try
             {
-                string [] words = txtWords.Text.Split('\n');
+                string[] words = txtWords.Text.Split('\n');
+                List <string> definitions = new List<string>();
+
                 foreach (string word in words ){
                 xDoc.Load("http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=0cec1fd4-d9d1-4ccc-9ad2-f996151173eb");
-                txtWords.Text = (xDoc.SelectSingleNode("entry_list/entry/def/dt").InnerText)+(Environment.NewLine);
-               
+                string definition = (xDoc.SelectSingleNode("entry_list/entry/def/dt").InnerText);
+                definitions.Add(word+definition);
+            }
+                txtWords.Text = String.Join("\n", definitions);
                 Clipboard.SetText(txtWords.Text);
                 lblCopied.Text = "Copied to the clipboard!";
             }
-            }
             catch
             {
-                MessageBox.Show("That is not a word in the dictionary, please try again.", "Word not found in the dictionary", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                MessageBox.Show("I wasn't able to find one of those words in the dictionary, please try again.", "Word not found in the dictionary", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             }
         
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnClr_Click(object sender, EventArgs e)
+        {
+            txtWords.Text = "";
         }
     }
 }
